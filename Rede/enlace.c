c0aeec849b95b5ab8054bcfd80afcb8b829aec42
Paolla Catherine <paollacath@gmail.com>
2015-09-04 12:10:30 -0300

    Comentando o código

--- Rede/enlace.c
+++ Rede/enlace.c
@@ -3,10 +3,40 @@
   * 11742244 Marcello da Silva Sousa 
   * 11742251 Náthia de Souza Marrafon
   * 11078359 Paolla Catherine S. M. Agard
+
+  * A biblioteca Enlace representa a Camada de Enlace na Emulação da Topologia
+  * da Rede. Os enlaces especificados no arquivo serão emulados através da 
+  * troca de datagramas UDP entre os processos que emulam os nós conectados na 
+  * rede virtual. Por exemplo, se o nó 1 estiver conectado ao  nó 2, qualquer 
+  * mensagem que o nó 1 enviar para o nó 2 deverá ser encapsulada em um datagrama 
+  * UDP e enviada para o processo que emula o nó 2 através de um soquete UDP. No 
+  * exemplo acima, o envio de um quadro do nó 1 para o nó 2 corresponderá ao envido 
+  * de um datagrama UDP para o par (IP,Porta) = (10.0.0.2, 5001).
+
+  * Considerando que a rede local é bastante confiável, não se espera a ocorrência 
+  * de erros de rede. Para observar-se erros na rede será introduzido um módulo 
+  * adulterador de pacotes imediatamente antes de enviar um datagrama UDP. O 
+  * adulterador irá introduzir os seguintes tipos de erros, de acordo com uma 
+  * dada probabilidade:
+  * - Perda do pacote
+  * - Corrupção de um ou mais bits do pacote
+  * - Duplicação do pacote
+
+  * Para usar o adulterador, deve-se trocar todas as instâncias de chamadas à primitiva 
+  * sendto, da API sockets, por sendto_garbled. Ambas funções têm exatamente os mesmos 
+  * argumentos.
+
+  * A biblioteca Enlace possui métodos de leitura de arquivo do tipo .cnf, exibição dos 
+  * nós e seus enlaces lidos no arquivo, verificar MTU e checksum, criar checksum, 
+  * inicializar camada de enlace, enviar mensagem para camada física e receber mensagem 
+  * da camada física.
 ***/
 
 #include "global.h"
 
+/**
+  * Esturura onde os nós e seus enlaces serão armazenados.
+***/
 typedef struct {
 	int id;
 	char ip[17];
@@ -14,8 +44,8 @@ typedef struct {
 	int neighbors[6];
 }node;
 
-
 node nodus[6];
+
 struct sockaddr_in physicalSend, physicalReceive, teste;
 
 
@@ -32,9 +62,28 @@ void *receivePhy(void *arg);
 
 
 /**
-  * Função para ler o arquivo,exibir na tela os nós e seus vizinhos
-  *
-  * param path: caminho do arquivo
+  * Lê um arquivo do tipo .cnf como o exemplo abaixo, para isso, o método recebe o 
+  * caminho do arquivo (char []), procura a palavra 'nos' e armazena o id, ip e porta 
+  * na struct nodus do tipo node, em seguida, procura a palavra 'enl', lê os enlaces 
+  * e com seus repectivos MTUs e armazena também na variável nodus no campo neighbors 
+  * exemplo: 1 -> 2, MTU = 2000; no nó de id 1 no campo neighbors indexado por 2-1 já 
+  * que os índices começam por 0 o valor 2000, que representa o MTU será armazenado, 
+  * isso será feito até a palavra 'fim' ser encontrada. 
+
+  * // topologia1.cnf
+
+  * Nos
+  *    1: IP = 10.0.0.2, Porta = 5000;
+  *    2: IP = 10.0.0.2, Porta = 5001;
+  *    3: IP = 10.0.0.3, Porta = 5000;
+  *    4: IP = 10.0.0.4, Porta = 5000;
+
+  * Enlaces
+  *    1 -> 2, MTU = 2000;
+  *    2 -> 3, MTU = 5000;
+  *    2 -> 4, MTU = 2000;
+  *    3 -> 4, MTU = 2000;
+  * Fim 
 ***/
 void readFile(char path[]) {
 	FILE *file;
@@ -146,7 +195,22 @@ void readFile(char path[]) {
 
 
 /**
-  * Função para exibir na tela os nós e seus vizinhos
+  * Exibe na tela os nós com seus respectivos enlaces, usando o arquivo acima como 
+  * exemplo será exibido na tala o seguinte:
+
+  * 1: IP = 10.0.0.2, Porta = 5000
+  *      1 -> 2, MTU = 2000
+
+  * 2: IP = 10.0.0.2, Porta = 5001
+  *      2 -> 3, MTU = 5000
+  *      2 -> 4, MTU = 2000
+
+  * 3: IP = 10.0.0.3, Porta = 5000
+  *      3 -> 4, MTU = 2000
+
+  * 4: IP = 10.0.0.4, Porta = 5000
+
+  * Observação: O método printDataLink atualmente não está sendo utilizado.
 ***/
 void printDataLink() {
 	int indexA, indexB;
@@ -166,21 +230,21 @@ void printDataLink() {
 
 
 /**
-  * Função para verificar o MTU
-  *
-  * param package: Pacote
-  * param mtu: MTU máximo que pode ser recebido/enviado
+  * Verifica se o MTU é maior ou igual ao tamanho do pacote que está sendo enviado, 
+  * para isso, o método recebe o pacote (char []) e o valor do MTU, se o tamanho do 
+  * pacote for menor retornará falso(-1), caso contrário retornará verdadeiro(0).
 ***/
 int checkMTU(char package[], int mtu) {
 	return (strlen(package) > mtu) ? -1 : 0;
 }
 
 /**
-  * Função para adicionar a detecção de erros no final do pacote.
-  * Foi utilizado a função crypto <crypt.h> para criar uma chave única
-  * para cada pacote, essa chave é concatenada ao final do pacote
-  *
-  * param package: Pacote
+  * Cria o checksum, um código usado para verificar a integridade de dados transmitidos 
+  * através de um canal com ruídos, que será adicionado no final do pacote para 
+  * verificações futuras, para isso, o método recebe o pacote (char []), é utilizado o 
+  * método crypt da biblioteca crypt.h que gera uma chave a partir da combinação do 
+  * pacote de dados a ser enviado e a palavra 'keycrypt', a chave gerada possui 14 
+  * caracteres mas apenas 3 são adicionados ao fim do pacote.
 ***/
 void createChecksum(char package[]) {
 	char checksum[14];
@@ -190,12 +254,17 @@ void createChecksum(char package[]) {
 	strcat(package, checksum);
 }
 
+
 /**
-  * Função para verificar a detecção de erros no final do pacote.
-  * A chave presente no final do pacote é retirada e verifica-se 
-  * se é a mesma que foi adicionada anteriomente
-  *
-  * param package: Pacote
+  * Verifica se o checksum, um código usado para verificar a integridade de dados 
+  * transmitidos através de um canal com ruídos, que está no final do pacote de dados 
+  * que foi recebido é o mesmo que foi adicionado, para isso, o método recebe o pacote 
+  * (char [])  os últimos 3 caracteres que foram adiiconados no final do pacote são 
+  * retiradas e armazenados em uma variável, um novo checksum é gerado utilizando o 
+  * método crypt da biblioteca crypt.h que gera novamente a chave a partir da combinação
+  * do pacote de dados a ser enviado e a palavra 'keycrypt', verifica-se se a chave 
+  * gerada é a mesma que foi retirada anteriomenten do final do pacote, se as chaves 
+  * forem iguais retornará verdadeiro(0), caso contrário retornará falso(-1).
 ***/
 int checkChecksum(char package[]) {
 	char checksum1[4], checksum2[14];
@@ -213,9 +282,44 @@ int checkChecksum(char package[]) {
 }
 
 /**
-  * Função para enviar para camada Física. 
+  * Inicializa a camada de enlace, para isso, o método recebe como parâmetro o nó 
+  * destino (int) e o caminho do arquivo (char []), para o método [readFile] é passado 
+  * o caminho do arquivo, em seguida, as thread tSend (enviar) e tReceive (receber) 
+  * são criadas com o método da biblioteca pthread.h pthread_create, para que essas 
+  * funções sejam executadas simultâneamente, após a execuçãos ambas terão suas 
+  * atividades suspensas com o método pthread_join da mesma biblioteca pthread.h.
+
+  * Obsercação: Nó destino (int) parâmetro não tilizado, será retirado ou utilizado na 
+  * sequência do projeto.
+***/
+int initDataLink(int nodeSrc, char path[] ){
+	
+	readFile(path);
+
+	if(pthread_create(&tSend, NULL, sendPhy, NULL)){
+		printf("Erro ao criar a thread para enviar\n");
+		return -1;
+	}
+
+	if(pthread_create(&tReceive, NULL, receivePhy, NULL)){
+		printf("Erro ao criar a thread para receber\n");
+		return -1;
+	}
+
+	if(pthread_join(tSend, NULL)!=0){
+		printf("Erro Thread Join\n");
+	}
+
+	if(pthread_join(tReceive, NULL)!=0){
+		printf("Erro Thread Join\n");
+	}
+}
+
+
+/**
+  * Função para recever da camada Física. 
   *
-  * param arg: Nó destino
+  * param arg: Nó origem
 ***/
 void *sendPhy(){
 	char package[100];
@@ -294,44 +398,11 @@ void *sendPhy(){
 	}
 }
 
-
 /**
   * Função para inicializar a camada Enlace. 
   *
   * param nodeSrc: Nó destino
   * param path: Caminho do arquivo
-***/
-int initDataLink(int nodeSrc, char path[] ){
-
-	
-	readFile(path);
-
-
-
-	if(pthread_create(&tSend, NULL, sendPhy, NULL)){
-		printf("Erro ao criar a thread para enviar\n");
-		return -1;
-	}
-
-	if(pthread_create(&tReceive, NULL, receivePhy, NULL)){
-		printf("Erro ao criar a thread para receber\n");
-		return -1;
-	}
-
-	if(pthread_join(tSend, NULL)!=0){
-		printf("Erro Thread Join\n");
-	}
-
-	if(pthread_join(tReceive, NULL)!=0){
-		printf("Erro Thread Join\n");
-	}
-}
-
-
-/**
-  * Função para recever da camada Física. 
-  *
-  * param arg: Nó origem
 ***/
 void *receivePhy(void *arg){
 	nodeDst=1;
